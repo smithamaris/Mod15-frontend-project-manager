1ccb057fe0493358fa78b931e2eb749873add3b5
smithamaris <smith.amaris@gmail.com>
2025-12-05 16:41:14 -0500

    env

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ProjectsPage from "./pages/ProjectPages"
 import Navbar from "./components/Navbar";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 
+// console.log(import.meta.env.VITE_BACKEND_URL);
 
 
 function App() {
--- src/clients/api.ts
+++ src/clients/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
         Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzMxZDM4NDU2ZGU4YTMwNmE5ODUyMCIsInVzZXJuYW1lIjoiU2hheSIsImVtYWlsIjoidXNlcjZAdGVzdC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjQ5NTc1MjcsImV4cCI6MTc2NDk2NDcyN30.tsnItzv6c2qjwN1xNIY0d5hPOGtYZkrDWN-LdQuIkQ0'
     }
